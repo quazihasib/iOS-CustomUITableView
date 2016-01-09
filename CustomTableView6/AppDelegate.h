c5ffa045a68a06460bc8fc0225c701af39a7dffe
Quazi Ridwan Hasib <quazihasib@quazis-macbook-pro.local>
2016-01-09 18:14:29 +1100

    Initial Commit

---
+++ CustomTableView6/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomTableView6
+//
+//  Created by Quazi Ridwan Hasib on 9/01/2016.
+//  Copyright © 2016 Quazi Ridwan Hasib. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomTableView6/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomTableView6
+//
+//  Created by Quazi Ridwan Hasib on 9/01/2016.
+//  Copyright © 2016 Quazi Ridwan Hasib. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
